36eb115fe28c68d0d98546aaa4e36246ce242c91
Mantas Kvajauskas <mantinasmantas@gmail.com>
2023-09-19 01:16:15 -0700

    darbas

---
+++ kintamieji/index.js
@@ -0,0 +1,11 @@
+/*
+
+    KINTAMIEJI
+
+
+ - number 
+ - string (tekstas)
+ - boolean (true/false)
+ - array ( masyvas / kolekcija / sarasas / listas / matrija / arejas)
+ - object ( objektas - kompleksinei informacijai aprasyti)
+   */
---
+++ number.js
@@ -0,0 +1,29 @@
+/* 
+
+  NUMBER
+
+  Teigiami / Neigiami
+  Sveikieji / Desimtainiai
+  Normalus / nenormalus
+
+  Iniciavimo budas
+  - const (default )
+ - let ( jei keiciasi informacija)
+  - var (niekada)
+
+
+
+  */
+
+
+
+  const amzius = 99;
+  let pinigine = 0;
+  var minusas = -7.56;
+
+  const x1 =  1;
+  const x2 = 2;
+  const x3 = 3;
+  const x4 = 4;
+  const x5 = 5;
+  
